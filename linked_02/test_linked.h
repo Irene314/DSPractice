daaa6332632c4c01e076cd02a3a122662e55fc26
Irene314 <yulin96413@gmail.com>
2020-11-07 10:11:41 +0800

    Create test_linked.h

---
+++ linked_02/test_linked.h
@@ -0,0 +1,21 @@
+
+#ifndef __TEST_LINKED_H__
+#define __TEST_LINKED_H__
+
+#include <string>
+
+class TestLinked {
+    public:
+        void test_append_data();
+        void test_contain_data();
+        void test_remove_data();
+        void test_reverse_data();
+        void test_merge_data();
+
+    private:
+        void error(std::string);
+};
+
+#endif
+
+// test_linked.h
