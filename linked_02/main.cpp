ad8243bfd293dc20397d2f4b466074398b9e4efe
Irene314 <yulin96413@gmail.com>
2020-11-07 10:11:36 +0800

    Create main.cpp

---
+++ linked_02/main.cpp
@@ -0,0 +1,18 @@
+
+#include "linked.h"
+#include "test_linked.h"
+
+int main(int argc, char *argv[]) {
+    Linked linked{10};
+    TestLinked test;
+
+    linked.dump_list();
+
+ //   test.test_append_data();
+ //   test.test_contain_data();
+ //   test.test_remove_data();
+    test.test_reverse_data();
+ //   test.test_merge_data();
+} // main()
+
+// main.cpp
