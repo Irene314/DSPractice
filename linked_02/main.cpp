ec19e4a3682ad324e7cfa2c974c9a4fe690d4728
Irene314 <yulin96413@gmail.com>
2020-12-12 23:25:34 +0800

    Update main.cpp

--- linked_02/main.cpp
+++ linked_02/main.cpp
@@ -4,6 +4,8 @@
 
 int main(int argc, char *argv[]) {
     Linked linked{10};
+    Linked linkedL{9};
+    Linked linkedM{11};
     TestLinked test;
 
     linked.dump_list();
@@ -11,8 +13,8 @@ int main(int argc, char *argv[]) {
  //   test.test_append_data();
  //   test.test_contain_data();
  //   test.test_remove_data();
-    test.test_reverse_data();
- //   test.test_merge_data();
+ //   test.test_reverse_data();
+    test.test_merge_data();
 } // main()
 
 // main.cpp
