9611351707fb79730cc24ee8200c9380f06c814f
Irene314 <yulin96413@gmail.com>
2020-12-12 23:25:32 +0800

    Update linked.h

--- linked_02/linked.h
+++ linked_02/linked.h
@@ -3,14 +3,13 @@
 #define __LINKED_H__
 
 
-struct Node {
+class Node {
+    friend class Linked;
     Node *next; // 指向下一個 node
-
     int data;   // 實際資料
 };
 
 // node.h
-
 
 class Linked {
     public:
