385b1f549ec3428bd0f66f98539e10eec22e8017
Irene314 <yulin96413@gmail.com>
2020-12-25 21:36:46 +0800

    Create node.h

---
+++ binary tree/node.h
@@ -0,0 +1,22 @@
+class Node {
+	public:
+		Node(int);
+		~Node();
+
+	public:
+		Node *root();
+		void addChild(Node *, Node *);
+		void cut(Node *);
+		void paste(Node *, Node *);
+		Node *search_breadth(int);
+		Node *search_depth_pre(int);
+		Node *search_depth_in(int);
+		Node *search_depth_post(int);
+
+    public:
+        int data;
+
+	private:
+		Node *left;
+		Node *right;
+};
