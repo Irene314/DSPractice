12a0d3f1fcea27e1c0020122f7c0290aae6557b2
Irene314 <yulin96413@gmail.com>
2020-12-25 21:37:17 +0800

    Create main.cpp

---
+++ binary tree/main.cpp
@@ -0,0 +1,22 @@
+#include "node.h"
+
+int main(int argc, char *argv[]) {
+
+
+
+
+
+
+
+
+/*    Linked linked{10};
+
+
+    linked.dump_list();
+
+    test.test_append_data();
+//    test.test_contain_data();
+//    test.test_remove_data();*/
+
+} // main()
+
