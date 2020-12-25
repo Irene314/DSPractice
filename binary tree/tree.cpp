f992119b6bfb4c85b4d15f3328ba8de1d45325f5
Irene314 <yulin96413@gmail.com>
2020-12-25 21:36:40 +0800

    Create tree.cpp

---
+++ binary tree/tree.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+#include "node.h"
+
+Node::Node(int data) {
+
+}
+
+Node::~Node() {
+
+}
+
+Node *Node::root(){
+
+}
+
+void Node::addChild(Node *p, Node *n){
+
+}
+
+void Node::cut(Node *t){
+
+}
+
+void Node::paste(Node *n, Node *t){
+
+}
+
+Node *Node::search_breadth(int data){
+
+}
+
+Node *Node::search_depth_pre(int data){
+
+}
+
+Node *Node::search_depth_in(int data){
+
+}
+
+Node *Node::search_depth_post(int data){
+
+}
