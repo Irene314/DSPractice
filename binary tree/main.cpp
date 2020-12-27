1e09ac0003efc7e0c1b413caf0c21d70fcd09004
Irene314 <yulin96413@gmail.com>
2020-12-27 16:55:50 +0800

    Update main.cpp

--- binary tree/main.cpp
+++ binary tree/main.cpp
@@ -3,18 +3,6 @@
 #include "node.h"
 
 int main(int argc, char *argv[]) {
-    Node *n1 = new Node();
-    Node *n2 = new Node();
-    Node *n3 = new Node();
-    Node *n4 = new Node();
-    Node *n5 = new Node();
-    Node *n6 = new Node();
-    Node *n7 = new Node();
-    std::cin >> n1->data >> n2->data >> n3->data >> n4->data >> n5->data >> n6->data >> n7->data;
-
-
-
-
 
 
 
