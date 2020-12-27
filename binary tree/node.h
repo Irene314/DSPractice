9b614a09bf7fb3c63797d788a67cb9980435487c
Irene314 <yulin96413@gmail.com>
2020-12-27 17:25:06 +0800

    Update node.h

--- binary tree/node.h
+++ binary tree/node.h
@@ -4,10 +4,10 @@ class Node {
 		void addChild(Node *, Node *);
 		void cut(Node *);
 		void paste(Node *, Node *);
-		void *search_breadth(Node *);
-		void *search_preorder(Node *);
-		void *search_inorder(Node *);
-		void *search_postorder(Node *);
+		void search_breadth(Node *);
+		void search_preorder(Node *);
+		void search_inorder(Node *);
+		void search_postorder(Node *);
 
     public:
         int data;
