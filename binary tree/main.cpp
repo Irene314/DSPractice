6743526ccc18979e6150c31353593feca52af197
Irene314 <yulin96413@gmail.com>
2020-12-27 17:22:26 +0800

    Update main.cpp

--- binary tree/main.cpp
+++ binary tree/main.cpp
@@ -3,17 +3,29 @@
 #include "node.h"
 
 int main(int argc, char *argv[]) {
+    Node *n1 = new Node();
+    Node *n2 = new Node();
+    Node *n3 = new Node();
+    Node *n4 = new Node();
+    Node *n5 = new Node();
+    Node *n6 = new Node();
+    Node *n7 = new Node();
 
+    std::cout << "Please input node data: " << std::endl;
+    std::cin >> n1->data >> n2->data >> n3->data >> n4->data >> n5->data >> n6->data >> n7->data;
 
+    Node *root_ = n1;
+    n1->left = n2;
+    n1->right = n3;
+    n2->left = n4;
+    n2->right = n5;
+    n3->left = n6;
+    n3->right = n7;
 
-/*    Linked linked{10};
-
-
-    linked.dump_list();
-
-    test.test_append_data();
-//    test.test_contain_data();
-//    test.test_remove_data();*/
+    void *search_breadth(n1);
+//    void *search_preorder(n1);
+//    void *search_inorder(n1);
+//    void *search_postorder(n1);
 
 } // main()
 
