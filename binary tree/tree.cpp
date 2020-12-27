05360ca3e3d039e40f058fd2f42389c01c056623
Irene314 <yulin96413@gmail.com>
2020-12-27 17:22:31 +0800

    Update tree.cpp

--- binary tree/tree.cpp
+++ binary tree/tree.cpp
@@ -2,29 +2,6 @@
 
 #include "node.h"
 
-Node::Node() {
-    Node *n1 = new Node();
-    Node *n2 = new Node();
-    Node *n3 = new Node();
-    Node *n4 = new Node();
-    Node *n5 = new Node();
-    Node *n6 = new Node();
-    Node *n7 = new Node();
-    std::cin >> n1->data >> n2->data >> n3->data >> n4->data >> n5->data >> n6->data >> n7->data;
-
-    Node *root_ = n1;
-    n1->left = n2;
-    n1->right = n3;
-    n2->left = n4;
-    n2->right = n5;
-    n3->left = n6;
-    n3->right = n7;
-}
-
-Node::~Node() {
-
-}
-
 Node *Node::root(){
     Node *root_ = new Node();
     return root_;
