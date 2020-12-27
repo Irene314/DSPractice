07f7b9f705facc554d0dd32ef3ab065cf75d830e
Irene314 <yulin96413@gmail.com>
2020-12-27 16:55:55 +0800

    Update tree.cpp

--- binary tree/tree.cpp
+++ binary tree/tree.cpp
@@ -3,7 +3,22 @@
 #include "node.h"
 
 Node::Node() {
+    Node *n1 = new Node();
+    Node *n2 = new Node();
+    Node *n3 = new Node();
+    Node *n4 = new Node();
+    Node *n5 = new Node();
+    Node *n6 = new Node();
+    Node *n7 = new Node();
+    std::cin >> n1->data >> n2->data >> n3->data >> n4->data >> n5->data >> n6->data >> n7->data;
 
+    Node *root_ = n1;
+    n1->left = n2;
+    n1->right = n3;
+    n2->left = n4;
+    n2->right = n5;
+    n3->left = n6;
+    n3->right = n7;
 }
 
 Node::~Node() {
@@ -16,29 +31,50 @@ Node *Node::root(){
 }
 
 void Node::addChild(Node *p, Node *n){
-
+    if(n->data > p->data)
+        p->right = n;
+    else
+        p->left = n;
 }
 
 void Node::cut(Node *t){
-
+    Node *root_1 = new Node();
+    root_1 = t;
 }
 
 void Node::paste(Node *n, Node *t){
+    Node *root_1 = new Node();
+    root_1 = t;
+    if(t->data > n->data)
+        n->right = t;
+    else
+        n->left = t;
+}
+
+void *Node::search_breadth(Node *p){
 
 }
 
-Node *Node::search_breadth(int data){
-
+void *Node::search_preorder(Node *p){
+    if(p) {
+        std::cout << p->data << " ";
+        search_preorder(p->left);
+        search_preorder(p->right);
+    }
 }
 
-Node *Node::search_depth_pre(int data){
-
+void *Node::search_inorder(Node *p){
+    if(p) {
+        search_inorder(p->left);
+        std::cout << p->data << " ";
+        search_inorder(p->right);
+    }
 }
 
-Node *Node::search_depth_in(int data){
-
-}
-
-Node *Node::search_depth_post(int data){
-
+void *Node::search_postorder(Node *p){
+    if(p) {
+        search_postorder(p->left);
+        search_postorder(p->right);
+        std::cout << p->data << " ";
+    }
 }
